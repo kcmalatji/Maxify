27a72b4c431572392db33e44ffeb41ce9df7f350
Chris <kcmalatji@gmail.com>
2018-08-18 20:17:16 +0200

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,20 +1,48 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { ModalController, NavParams } from 'ionic-angular';
-//import { NewTripPage } from '../new-trip/new-trip';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  map:any=null;
+  geoInfo:any={
+      resp:'',
+      data:''
+  };
+  lat : number;
+  longt: number;
 
-  constructor(public navCtrl: NavController,public modal:ModalController) {
+  constructor(public navCtrl: NavController,private geolocation: Geolocation) {
+ 
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.geoInfo.resp=JSON.stringify(resp);
+       resp.coords.latitude
+       resp.coords.longitude
+       this.lat = (resp.coords.latitude);
+       this.longt =(resp.coords.longitude);
+       console.log(this.lat);
+       console.log(this.longt);
+
+  }).catch((error) => {
+      console.log('Error getting location', error);
+      this.geoInfo.resp='Error getting location';
+  });
+
+  let watch = this.geolocation.watchPosition();
+  watch.subscribe((data) => {
+      this.geoInfo.data=JSON.stringify(data);
+      // data can be a set of coordinates, or an error (if an error occurred).
+       data.coords.latitude
+       data.coords.longitude
+  });
 
   }
-  openModal(){
-    let newTrip=this.modal.create('NewTripPage');
-    newTrip.present();
+  
+  test(){
+ 
   }
 
 }
